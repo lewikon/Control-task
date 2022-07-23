6cf0a7b990854943cf8c3e9f4849ce4900678eec
Alina <lewibein@icloud.com>
2022-07-23 18:51:02 +0300

    upgrade code

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
-﻿string[] input = { "еда", "стагетти", "соус", "поле", "лес" };
-
-string output = FindShortWords(input);
-
-string FindShortWords(string[] input)
+﻿string[] input = { "еда", "стагетти", "соус", "поле", "лес" };int FindShortWords(string[] input)
 {
-    string output = string.Empty;
+    int  count = 0;
     for (int i = 0; i < input.Length; i++)
     {
         string temp = string.Empty;
         temp = input[i];
         if (temp.Length <= 3)
         {
-            output = output + temp + " ";
+            count++;
         }
 
     }
-    return output;
+    return count;
 }
-Console.WriteLine(output);
