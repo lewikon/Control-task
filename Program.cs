e728121c33cd925c8a1698777a1ca17ad7c0cb47
Alina <lewibein@icloud.com>
2022-07-23 15:31:10 +0300

    create main task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = { "еда", "стагетти", "соус", "поле", "лес" };
+string output = FindShortWords(input);
+string FindShortWords(string[] input)
+{
+    string output = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = string.Empty;
+        temp = input[i];
+        if (temp.Length <= 3)
+        {
+            output = output + temp + " ";
+        }
+
+    }
+    return output;
+}
+Console.WriteLine(output);
