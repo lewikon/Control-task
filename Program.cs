7dc21c7b8f7067f143ffcfe6e2a9a312eb21d301
Alina <lewibein@icloud.com>
2022-07-24 17:30:28 +0300

    upgrade method

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
-﻿string[] input = { "еда", "стагетти", "соус", "поле", "лес" };int FindShortWords(string[] input)
+﻿string[] input =
+{ "90", "еда", "стагетти", "соус", "поле", "лес", "папарацци"};
+Console.WriteLine(PrintArray(WordCount(input)));
+
+string[] WordCount(string[] input)
 {
-    int  count = 0;
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temporary = string.Empty;
+        temporary = input[i];
+        if (temporary.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] output = new string[count];
+    int index = 0;
     for (int i = 0; i < input.Length; i++)
     {
         string temp = string.Empty;
         temp = input[i];
         if (temp.Length <= 3)
         {
-            count++;
+            output[index] = temp;
+            index++;
         }
-
     }
-    return count;
+    return output;
+
+}
+string PrintArray(string[] array)
+{
+    string output = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        output = output + array[i] + " ";
+    }
+    return output;
 }
